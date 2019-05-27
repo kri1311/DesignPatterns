311c8b465ae2ea9c03c1e76bbec29274d1608d9d
Krishna Tiwari <kri1311@gmail.com>
2019-05-27 21:51:19 +0530

    Update Bike.java

--- VisitorPattern/Bike.java
+++ VisitorPattern/Bike.java
@@ -1,1 +1,29 @@
 
+package com.practice.example;
+
+
+class Bike implements Vehicle{
+	int price;
+	float weight;
+	String Color = "BLUE";
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	public String getColor() {
+		return Color;
+	}
+	public void setColor(String color) {
+		Color = color;
+	}
+	
+	public void visit(VehicleDisplayVisitor a) {}
+}
