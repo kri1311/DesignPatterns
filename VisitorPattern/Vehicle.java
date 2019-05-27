57cf3d37f2bfa0537b9e7a22c02bb3829b0dff7b
Krishna Tiwari <kri1311@gmail.com>
2019-05-27 21:56:33 +0530

    Update Vehicle.java

--- VisitorPattern/Vehicle.java
+++ VisitorPattern/Vehicle.java
@@ -3,7 +3,7 @@ package com.practice.example;
 public interface Vehicle {
 	
 	public void accept(VehicleVisitor v);
-	public void visit(VehicleDisplayVisitor a);
+	//public void visit(VehicleDisplayVisitor a);
 	
 	
 }
