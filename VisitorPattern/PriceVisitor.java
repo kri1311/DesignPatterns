e9137fe5abf44b2c561c37d3053094781b424ba7
Krishna Tiwari <kri1311@gmail.com>
2019-05-29 17:49:57 +0530

    Update PriceVisitor.java

--- VisitorPattern/PriceVisitor.java
+++ VisitorPattern/PriceVisitor.java
@@ -4,6 +4,9 @@ public class PriceVisitor implements VehicleVisitor{
 	
 	public void visit(Car car) {
 		System.out.println("Calculating the price of the Car");
+		int price=car.getPrice();
+		System.out.println(price);
+		
 	}
 	public void visit(Bike bike) {
 		System.out.println("Calculating the price of the Bike");
