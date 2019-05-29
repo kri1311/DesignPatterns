b794896f7c73875c4faabee12825240d2df19eda
Krishna Tiwari <kri1311@gmail.com>
2019-05-29 17:30:23 +0530

    Update VisitorPatternDemo.java

--- VisitorPattern/VisitorPatternDemo.java
+++ VisitorPattern/VisitorPatternDemo.java
@@ -2,6 +2,12 @@ public class VisitorPatternDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Vehicle v = new Car(100.0,10000,’Blue’);
+		Vehicle v = new Bike(50.0,300,’Yellow’);
+		Vehicle v = new Truck(90.0,5000,’Green’);
+
+		
 
 	}
 
