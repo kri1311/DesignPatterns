f5e7ba4ae0b10ab972cd69d71790c408a340af02
Krishna Tiwari <kri1311@gmail.com>
2019-06-07 16:20:29 +0530

    Update ElementFactory.java

--- FactoryPattern/UserInterface/ElementFactory.java
+++ FactoryPattern/UserInterface/ElementFactory.java
@@ -1,1 +1,23 @@
 
+package com.practice.example;
+
+public class ElementFactory implements Element {
+	private String elementType;
+	public void getElement() {
+		Element e =  null;
+	
+	
+	if(elementType=="button") {
+		return new Button();
+	}
+	
+	if(elementType=="label") {
+		return new Label();
+	}
+	
+	if(elementType=="textField") {
+		return new TextField();
+	}
+
+	}
+}
