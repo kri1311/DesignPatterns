ddeac1d8c1df2d728dda59f5270f768d4ee48c4b
Krishna Tiwari <kri1311@gmail.com>
2019-06-07 16:21:59 +0530

    Update Label.java

--- FactoryPattern/UserInterface/Label.java
+++ FactoryPattern/UserInterface/Label.java
@@ -1,1 +1,25 @@
 
+package com.practice.example;
+
+public class Label extends Element {
+	
+	private int labelID;
+	private String labelName;
+	public int getLabelID() {
+		return labelID;
+	}
+	public void setLabelID(int labelID) {
+		this.labelID = labelID;
+	}
+	public String getLabelName() {
+		return labelName;
+	}
+	public void setLabelName(String labelName) {
+		this.labelName = labelName;
+	}
+	
+	public void buildElement(String label) {
+		System.out.println("Constructing the Label in UI"+label);
+	}
+
+}
