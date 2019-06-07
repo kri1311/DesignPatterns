9054aaf8efdaeb5cec8be2a06e231a5a1dd52bdc
Krishna Tiwari <kri1311@gmail.com>
2019-06-07 16:21:42 +0530

    Update FactoryPatternDemo.java

--- FactoryPattern/UserInterface/FactoryPatternDemo.java
+++ FactoryPattern/UserInterface/FactoryPatternDemo.java
@@ -1,1 +1,17 @@
 
+package com.practice.example;
+
+public class FactoryPatternDemo extends Element{
+	
+	public static void main(String args[])
+	{
+		ElementFactory elementFactory = new ElementFactory();
+		Element button = elementFactory.buildElement("button");
+		Element label = elementFactory.buildElement("label");
+		Element textField = elementFactory.buildElement("textField");
+		
+		
+		
+	}
+}
+
