e171af594b711b1245a05248b4d80e9136f39bbc
Krishna Tiwari <kri1311@gmail.com>
2019-06-07 16:19:29 +0530

    Update Button.java

--- FactoryPattern/UserInterface/Button.java
+++ FactoryPattern/UserInterface/Button.java
@@ -1,1 +1,32 @@
 
+package com.practice.example;
+public class Button extends Element {
+	
+	private int buttonID;
+	private String buttonName;
+	private String buttontype;
+	public int getButtonID() {
+		return buttonID;
+	}
+	public void setButtonID(int buttonID) {
+		this.buttonID = buttonID;
+	}
+	public String getButtonName() {
+		return buttonName;
+	}
+	public void setButtonName(String buttonName) {
+		this.buttonName = buttonName;
+	}
+	public String getButtontype() {
+		return buttontype;
+	}
+	public void setButtontype(String buttontype) {
+		this.buttontype = buttontype;
+	}
+	
+	public void buildElement(String button) {
+		System.out.println("Constructing the button in UI"+button);
+	}
+	
+
+}
