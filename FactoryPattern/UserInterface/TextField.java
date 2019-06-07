73354c12be24ea4696ac069e8a3758880d50a981
Krishna Tiwari <kri1311@gmail.com>
2019-06-07 16:22:23 +0530

    Update TextField.java

--- FactoryPattern/UserInterface/TextField.java
+++ FactoryPattern/UserInterface/TextField.java
@@ -1,1 +1,35 @@
 
+package com.practice.example;
+
+public class TextField extends Element {
+	
+	private int textFieldID;
+	private String textFieldName;
+	private int textFieldLength;
+	public int getTextFieldID() {
+		return textFieldID;
+	}
+	public void setTextFieldID(int textFieldID) {
+		this.textFieldID = textFieldID;
+	}
+	public String getTextFieldName() {
+		return textFieldName;
+	}
+	public void setTextFieldName(String textFieldName) {
+		this.textFieldName = textFieldName;
+	}
+	public int getTextFieldLength() {
+		return textFieldLength;
+	}
+	public void setTextFieldLength(int textFieldLength) {
+		this.textFieldLength = textFieldLength;
+	}
+	
+	public void buildElement(String textField) {
+		
+		System.out.println("Constructing the TextField in UI"+textField);
+		
+	}
+	
+
+}
