af8b369e02cc547337cc4e2d18c459e138c82a07
Krishna Tiwari <kri1311@gmail.com>
2019-06-07 17:21:34 +0530

    Create TransportApplication.java

---
+++ VisitorPattern/TransportApplication.java
@@ -0,0 +1,24 @@
+public class TransportApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Vehilce> vehilces=new ArrayList<Vehicle>();
+		//Add elements to the list;
+		
+		
+		
+		Vehicle car = new Car(100.0,10000,’Blue’);
+		Vehicle bike = new Bike(50.0,300,’Yellow’);
+		Vehicle truck = new Truck(90.0,5000,’Green’);
+		
+		// Perform some operations. 
+		//Junit Test Cases : 
+		//set and get color 
+		//How to test visitors ?
+		
+
+		
+
+	}
+
+}
