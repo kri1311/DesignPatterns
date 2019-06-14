86322e2bae32eb44e376b39f3d9dbb78630b95d4
Krishna Tiwari <kri1311@gmail.com>
2019-06-14 15:10:28 +0530

    Create VisitorPatternDemo.java

---
+++ VisitorPattern/VisitorPatternDemo.java
@@ -0,0 +1,38 @@
+package com.practice.example;
+
+import java.util.ArrayList;
+
+
+public class VisitorPatternDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
+		//Initializing vehicles
+		vehicles.add(new Car("BMW",100,90.25,"BLUE"));
+		vehicles.add(new Car("Audi",110,80.50,"GREEN"));
+		vehicles.add(new Car("Honda",120,45.23,"YELLOW"));
+		
+		vehicles.add(new Bike("Hero",130,91.25,"BLUE"));
+		vehicles.add(new Bike("Jupiter",140,83.50,"GREEN"));
+		vehicles.add(new Bike("TVS",150,47.23,"YELLOW"));
+		
+		vehicles.add(new Truck("AshokLeyLand",210,97.25,"BLUE"));
+		vehicles.add(new Truck("Eicher Motors",220,89.50,"GREEN"));
+		vehicles.add(new Truck("Asia Motors",300,41.23,"YELLOW"));
+		
+		MaxPriceVisitor vv = new MaxPriceVisitor();
+		BlueColorVisitor bv = new BlueColorVisitor();
+		MaxWeightVisitor mpv = new MaxWeightVisitor();
+		
+		vv.findMaxPriceVehicle(vehicles);
+		bv.findBlueColorVehicles(vehicles);
+		mpv.findMaxWeightVisitor(vehicles);
+	}
+
+}
+
+
+
+
+
