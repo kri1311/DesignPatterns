47128fe924f2a13d27649253872e4971ff0b1e77
Krishna Tiwari <kri1311@gmail.com>
2019-07-26 16:30:40 +0530

    Update JavaAWT.java

--- AbstractFactoryPattern/UserInterface/JavaAWT.java
+++ AbstractFactoryPattern/UserInterface/JavaAWT.java
@@ -1,1 +1,30 @@
 
+package com.practice.example;
+
+public class Awt extends UIFactory {
+  
+	public Awt() {
+		//System.out.println("Inside Constructor");
+	}
+	    Element e = null;
+	    Element getElement(String eType) {
+	    switch(eType) {
+			case "Button":	
+				e=(Element) new Button();
+				break;
+			case "Label":	
+				e=(Element) new Label();
+				break;
+			case "TextField":	
+				e=(Element) new TextField();
+				break;
+			}
+		return e;	
+	    }
+		@Override
+		protected Element getElement(ElementType button) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	
+}
