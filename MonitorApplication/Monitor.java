de90fac24364f9344461a401d450e913a3faada8
Krishna Tiwari <kri1311@gmail.com>
2019-07-26 16:51:26 +0530

    Create Monitor.java

---
+++ MonitorApplication/Monitor.java
@@ -0,0 +1,13 @@
+package com.example.practice;
+
+public class Monitor {
+	
+	public void monitor(String ip, int port, int freq, Client client) {
+		
+	}
+
+	public void register(String ip, int port, int freq, Client client) {
+		
+	}
+
+}
