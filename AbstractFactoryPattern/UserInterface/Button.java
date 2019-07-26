04da4624e7c745fc33553eb6beb2d395e1d41158
Krishna Tiwari <kri1311@gmail.com>
2019-07-26 16:31:25 +0530

    Update Button.java

--- AbstractFactoryPattern/UserInterface/Button.java
+++ AbstractFactoryPattern/UserInterface/Button.java
@@ -1,1 +1,33 @@
 
+package com.practice.example;
+
+public class Button implements Element {
+	
+	private int buttonID;
+	private String buttonName;
+	private String buttontype;
+	public int getButtonID() {
+		return buttonID;
+	}
+	public void setButtonID(int buttonID) {
+		this.buttonID = buttonID;
+	}
+	public String getButtonName() {
+		return buttonName;
+	}
+	public void setButtonName(String buttonName) {
+		this.buttonName = buttonName;
+	}
+	public String getButtontype() {
+		return buttontype;
+	}
+	public void setButtontype(String buttontype) {
+		this.buttontype = buttontype;
+	}
+	
+	public void buildElement() {
+		System.out.println("Constructing the button");
+	}
+	
+
+}
