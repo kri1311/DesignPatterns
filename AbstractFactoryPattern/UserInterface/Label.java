3b517ffff20a4a9f12369eec232f9e2770f8ae88
Krishna Tiwari <kri1311@gmail.com>
2019-07-26 16:36:39 +0530

    Update Label.java

--- AbstractFactoryPattern/UserInterface/Label.java
+++ AbstractFactoryPattern/UserInterface/Label.java
@@ -1,1 +1,26 @@
 
+package com.practice.example;
+
+
+public class Label implements Element {
+	
+	private int labelID;
+	private String labelName;
+	public int getLabelID() {
+		return labelID;
+	}
+	public void setLabelID(int labelID) {
+		this.labelID = labelID;
+	}
+	public String getLabelName() {
+		return labelName;
+	}
+	public void setLabelName(String labelName) {
+		this.labelName = labelName;
+	}
+	
+	public void buildElement() {
+		System.out.println("Constructing the Label");
+	}
+
+}
