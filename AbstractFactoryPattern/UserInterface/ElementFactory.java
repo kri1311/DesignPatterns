ec26190caed80fd91f22a3fc225148391b70446b
Krishna Tiwari <kri1311@gmail.com>
2019-07-26 16:32:23 +0530

    Create ElementFactory.java

---
+++ AbstractFactoryPattern/UserInterface/ElementFactory.java
@@ -0,0 +1,20 @@
+package com.practice.example;
+
+public class ElementFactory  {
+	public Element getElement(String ElementType) {
+		Element e =  null;
+		
+		switch(ElementType) {
+		case "Button":	
+			e=new Button();
+			break;
+		case "Label":	
+			e=new Label();
+			break;
+		case "TextField":	
+			e=new TextField();
+			break;
+		}
+	return e;	
+  }	
+}
