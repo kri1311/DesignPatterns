3f3657e3a9b66a785a9ec27950b4586d1bcdf5c7
Krishna Tiwari <kri1311@gmail.com>
2019-07-26 16:30:09 +0530

    Update AbstractFactoryPatternDemo.java

--- AbstractFactoryPattern/UserInterface/AbstractFactoryPatternDemo.java
+++ AbstractFactoryPattern/UserInterface/AbstractFactoryPatternDemo.java
@@ -1,1 +1,40 @@
 
+package com.practice.example;
+
+
+public class AbstractFactoryPatternDemo {
+	
+	public static void main(String args[])
+	{
+		// Getting Swing Factory
+		UIFactory factory = FactoryProvider.getFactory(UIType.SWING);
+       
+		
+      //Building UI using Swing Factory
+        Element e = factory.getElement(ElementType.BUTTON);
+        if (e != null) {
+        	//System.out.println("Hello.");
+            e.buildElement();
+        } else {
+            System.out.println("Element with given name doesn't exist.");
+        }
+        
+        
+        // Getting AWT Factory
+        factory = FactoryProvider.getFactory(UIType.AWT);
+        if (factory == null) {
+            System.out.println("Factory for given name doesn't exist.");
+            System.exit(1);
+        }
+		
+      //Building UI using AWT Factory
+        e = factory.getElement(ElementType.TEXTFIELD);
+        if (e != null) {
+        	System.out.println("Hello");
+            e.buildElement();
+        } else {
+            System.out.println("Element with given name doesn't exist.");
+        }
+        
+	}
+}
