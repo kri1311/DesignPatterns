0c2db7f14411c8e7cac4a5a7a6acec25c2f5905b
Krishna Tiwari <kri1311@gmail.com>
2019-08-23 14:39:36 +0530

    Update Monitor.java

--- MonitorApplication/Monitor.java
+++ MonitorApplication/Monitor.java
@@ -1,13 +1,32 @@
 package com.example.practice;
 
-public class Monitor {
-	
-	public void monitor(String ip, int port, int freq, Client client) {
-		
-	}
+import java.util.HashMap;
 
-	public void register(String ip, int port, int freq, Client client) {
+public class Monitor implements Runnable{
+	
+	private static HashMap<Service,Caller> hm = new HashMap<Service, Caller>();
+	private static Monitor instance;
+	
+	private Monitor() {}
+	public synchronized static Monitor getInstance() {
+		if(instance==null) {
+			instance = new Monitor();
+		}
+		return instance;
+	}
+	
+	
+	
+	
+	public void register(Service s,int freq, Caller client) {
 		
 	}
+	
+	public void run () {
+		
+	}
+	// Flyweight pattern for service class
+	
+	
 
 }
