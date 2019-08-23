e7e0bc15a5fd79d54656e797e4cfd2a0eaded061
Krishna Tiwari <kri1311@gmail.com>
2019-08-23 14:38:48 +0530

    Update Service.java

--- MonitorApplication/Service.java
+++ MonitorApplication/Service.java
@@ -1,32 +1,47 @@
 package com.example.practice;
 
+import java.awt.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Service {
 	
-	private String ip;
-	private int port;
-	private int freq;
+	private final String ip;
+	private final int  port;
+	private static Map<String, TreeMap<Integer, Service>> serviceCache = new TreeMap<String, TreeMap<Integer,Service>>();
+	
+	public static Service of(String ip,int port) {
+		//If service is existing in serviceCache, return it., otherwise create new one and return new one 
+		
+		if(serviceCache.containsKey(ip) && serviceCache.get(ip).containsKey(port) ) {
+			return serviceCache.get(ip).get(port);
+		}
+		else {
+			
+		    Service s = new Service(ip,port);
+		    serviceCache.putIfAbsent(ip, new TreeMap<Integer,Service>());
+		    serviceCache.get(ip).putIfAbsent(port, s);
+		    
+		    return s;
+		}
+		 
+	}
+	
+	
+	private Service(String ip,int port) {
+		this.ip = ip;
+		this.port = port;
+		
+	}
 	public String getIp() {
 		return ip;
 	}
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
+
 	public int getPort() {
 		return port;
 	}
-	public void setPort(int port) {
-		this.port = port;
-	}
-	public int getFreq() {
-		return freq;
-	}
-	public void setFreq(int freq) {
-		this.freq = freq;
-	}
-
-	public Service(String ip,int port,int freq) {
-		this.ip = ip;
-		this.port = port;
-		this.freq = freq;
-	}
+	
 }
+//make constructor private 
+// 
