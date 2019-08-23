9c239db463862818c8f859bcc8d4c45d22abb300
Krishna Tiwari <kri1311@gmail.com>
2019-08-23 15:58:24 +0530

    Update Monitor.java

--- MonitorApplication/Monitor.java
+++ MonitorApplication/Monitor.java
@@ -1,32 +1,45 @@
 package com.example.practice;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class Monitor implements Runnable{
-	
-	private static HashMap<Service,Caller> hm = new HashMap<Service, Caller>();
+public class Monitor implements Runnable {
+
+	private static HashMap<Service, Caller> hm = new HashMap<Service, Caller>();
 	private static Monitor instance;
-	
-	private Monitor() {}
+
+	private Monitor() {
+	}
+
 	public synchronized static Monitor getInstance() {
-		if(instance==null) {
+		if (instance == null) {
 			instance = new Monitor();
 		}
 		return instance;
 	}
-	
-	
-	
-	
-	public void register(Service s,int freq, Caller client) {
-		
+
+	/*
+	 * If a Caller is already registered for a service, skip it, else register it.
+	 * 
+	 */
+	public void register(Service s, int freq, Caller client) {
+
+		final Map<Caller, TreeMap<Service, Integer>> registerCache = new TreeMap<Caller, TreeMap<Service, Integer>>();
+
+		if (registerCache.containsKey(client) && registerCache.get(client).containsValue(s))
+			return;
+		else {
+			registerCache.putIfAbsent(client, new TreeMap<Service, Integer>());
+			registerCache.get(client).putIfAbsent(s, freq);
+		}
+
 	}
-	
-	public void run () {
-		
+
+	@Override
+	public void run() {
+
 	}
 	// Flyweight pattern for service class
-	
-	
 
 }
