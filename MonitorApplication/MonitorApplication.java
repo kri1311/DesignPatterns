7df859da9d52d2fdcafd5a3da8f9f0d487d693c5
Krishna Tiwari <kri1311@gmail.com>
2019-08-30 16:06:08 +0530

    Update MonitorApplication.java

--- MonitorApplication/MonitorApplication.java
+++ MonitorApplication/MonitorApplication.java
@@ -1,35 +1,44 @@
 package com.example.practice;
 
-public class MonitorApplication implements Caller  {
-	
-	public static void main(String args[]) {
-		
-		MonitorApplication mp =new MonitorApplication();
-		
-		
-		Service s = Service.of("9.114.96.120",443);
-		//Caller c = new Caller("Client A");
-		
-		Monitor m = Monitor.getInstance();
-		
-		//m.monitor("9.114.96.120",443,3, new Client());
+public class MonitorApplication implements Caller {
 
-		m.register(s,3, mp);
+	public static void main(String args[]) {
+
+		MonitorApplication mp = new MonitorApplication();
+
+		Service s = Service.of("127.0.0.1", 8080);
+
+		Monitor m = Monitor.getInstance();
+		m.checkServiceStatus(s, "127.0.0.1", 8080);
+
+		m.register(s, 3, mp);
 	}
 
 	@Override
 	public void serviceUp(Service s) {
 		// TODO Auto-generated method stub
-		
+		/*
+		 * If the connection is established, the service is up
+		 * 
+		 */
+		// if (serviceCache.containsKey(ip) && serviceCache.get(ip).containsKey(port))
+		// return true;
+		// else
+		// return false;
+		System.out.println("Service" + s.toString() + " is up");
+
 	}
 
 	@Override
 	public void serviceDown(Service s) {
 		// TODO Auto-generated method stub
-		
+		/*
+		 * If the connection doesn't established, the service is down
+		 * 
+		 */
+		/// Logic to notify callers
+		// return false;
+		System.out.println("Service" + s.toString() + " is down");
 	}
 
 }
-
-
-
