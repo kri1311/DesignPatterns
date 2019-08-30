1d195ee84c4d92e21874d72345efb7a2d77ed61d
Krishna Tiwari <kri1311@gmail.com>
2019-08-30 16:14:12 +0530

    Update Monitor.java

--- MonitorApplication/Monitor.java
+++ MonitorApplication/Monitor.java
@@ -1,13 +1,18 @@
 package com.example.practice;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Monitor implements Runnable {
 
-	private static HashMap<Service, Caller> hm = new HashMap<Service, Caller>();
+	// private static HashMap<Service, Caller> hm = new HashMap<Service, Caller>();
 	private static Monitor instance;
+	private Socket socket = null;
+	private DataInputStream input = null;
+	private DataOutputStream out = null;
 
 	private Monitor() {
 	}
@@ -23,23 +28,56 @@ public class Monitor implements Runnable {
 	 * If a Caller is already registered for a service, skip it, else register it.
 	 * 
 	 */
-	public void register(Service s, int freq, Caller client) {
+	// <Service, List of Callers (caller, frequency)>
 
-		final Map<Caller, TreeMap<Service, Integer>> registerCache = new TreeMap<Caller, TreeMap<Service, Integer>>();
+	// 2 service,1 caller 1,2
+	// Caller 1 : Google , freq : 1.5 sec --checking..
+	// Caller 2 : Google, freq : 2 sec
+	//
+	// 1.5 sec, 3 sec ,
 
-		if (registerCache.containsKey(client) && registerCache.get(client).containsValue(s))
-			return;
-		else {
-			registerCache.putIfAbsent(client, new TreeMap<Service, Integer>());
-			registerCache.get(client).putIfAbsent(s, freq);
+	// final Map<Service, List<Caller>> registerCache = new TreeMap<Service, List<Caller>>();
+	// final Map<Service, Integer> hm = new TreeMap<Service, Integer>();
+	//
+	// public void register(Service s, int freq, Caller client) {
+	//
+	// if (registerCache.containsKey(client) && registerCache.get(client).containsValue(s))
+	// return;
+	// else {
+	// registerCache.putIfAbsent(s, new TreeMap<Service, Integer>());
+	// registerCache.get(s).putIfAbsent(s, freq);
+	// }
+	//
+	// }
+
+	boolean checkServiceStatus(Service s, String address, int port) {
+		try {
+			socket = new Socket(address, port);
+			System.out.println("Connected");
+
+			// takes input from terminal
+			input = new DataInputStream(System.in);
+
+			// sends output to the socket
+			out = new DataOutputStream(socket.getOutputStream());
+			return true;
+		} catch (UnknownHostException u) {
+			System.out.println(u);
+		} catch (IOException i) {
+			System.out.println(i);
 		}
+
+		return false;
 
 	}
 
+	// public void plannedOutageRegistration(Service,Caller,int startTime, int endTime) {
+	//
+	// }
 	@Override
 	public void run() {
 
 	}
-	// Flyweight pattern for service class
+	
 
 }
