a2bcacfe9dd667ac6e86f7ad0576c030b6d9d912
Krishna Tiwari <kri1311@gmail.com>
2019-09-13 16:07:55 +0530

    Update MonitorApplicationTest.java

--- MonitorApplication/MonitorApplicationTest.java
+++ MonitorApplication/MonitorApplicationTest.java
@@ -5,15 +5,23 @@ import junit.framework.TestCase;
 public class MonitorApplicationTest extends TestCase {
 
 	// Test the Service IP
+	Service s = Service.of("172.217.16.163", 80);
+
 	public void testServiceIP() {
-		Service s = Service.of("172.217.16.163", 80);
+
 		assertTrue(s.getIp() == "172.217.16.163");
 	}
 
 	// Test the Service Port
 	public void testServicePort() {
-		Service s = Service.of("172.217.16.163", 80);
 		assertTrue(s.getPort() == 80);
+	}
+
+	public void testServiceStatus() {
+		Monitor m = Monitor.getInstance();
+		boolean serviceStatus = m.checkServiceStatus(s);
+		assertTrue(serviceStatus == true);
+
 	}
 
 }
