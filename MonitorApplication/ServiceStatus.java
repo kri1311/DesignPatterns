1d2c26efdc0ecbe94006313fb9488beb3bb4fab2
Krishna Tiwari <kri1311@gmail.com>
2019-11-22 17:28:45 +0530

    Update ServiceStatus.java

--- MonitorApplication/ServiceStatus.java
+++ MonitorApplication/ServiceStatus.java
@@ -1,20 +1,60 @@
 package com.monitor.service;
 
-import java.util.Timer;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.List;
 import java.util.TimerTask;
+import java.util.logging.Logger;
 
-class ServiceStatus extends TimerTask {
+public class ServiceStatus extends TimerTask {
+
+	private final Service s;
+	private final List<Caller> callers;
+	private boolean previousServiceState = true;
+	Logger logger = Logger.getLogger(MonitorApplication.class.getName());
+
+	// TODO - Include Servicestatus constructor
+	ServiceStatus(Service s, List<Caller> callers) {
+		System.out.println("ServiceStatus  constructor");
+		this.s = s;
+		this.callers = callers;
+	}
+
 	@Override
 	public void run() {
 		System.out.println("Checking service status");
+		boolean status = checkServiceStatus(s);
+		if (!status && previousServiceState) {
+			for (Caller caller : callers) {
+				caller.serviceDown(s);
+			}
+			previousServiceState = false;
+		} else if (status && !previousServiceState) {
+
+			for (Caller caller : callers) {
+				caller.serviceUp(s);
+			}
+			previousServiceState = true;
+		}
 
 	}
 
-	public boolean test(Service S) {
-		// main method
-		Timer t1 = new Timer();
-		t1.schedule(new ServiceStatus(), 0, 600);
+	private boolean checkServiceStatus(Service s) {
+		try {
+			// Socket socket = new Socket(s.getIp(), s.getPort());
+			Socket socket = new Socket();
+			socket.connect(new InetSocketAddress(s.getIp(), s.getPort()), 2000);
+			// System.out.println("Connection to Service (" + s.getIp() + ":" + s.getPort() + ") Established."); // should be part of logger with different level
+			logger.info("Connection to Service (" + s.getIp() + ":" + s.getPort() + ") Established.");
 
-		return true;
+			socket.close();
+
+			return true;
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return false;
+
 	}
 }
