e4ab7552a35fc6091c157e73ad66e1ad066b28b7
Krishna Tiwari <kri1311@gmail.com>
2020-01-24 17:21:05 +0530

    Update ServiceStatus.java

--- MonitorApplication/ServiceStatus.java
+++ MonitorApplication/ServiceStatus.java
@@ -14,24 +14,26 @@ public class ServiceStatus extends TimerTask {
 	private final List<Caller> callers;
 	private boolean previousServiceState = true;
 	private final Map<Caller, PlannedServiceOutage> pso;
+	private final int graceTime;
 
 	Logger logger = Logger.getLogger(MonitorApplication.class.getName());
 	// PannedServiceOutage obj - new PlannedServiceOutage();
 
 	// TODO - Include Servicestatus constructor
-	ServiceStatus(Service s, List<Caller> callers, Map<Caller, PlannedServiceOutage> pso) {
+	ServiceStatus(Service s, List<Caller> callers, Map<Caller, PlannedServiceOutage> pso,int graceTime) {
 		// System.out.println("ServiceStatus constructor");
 		logger.info("ServiceStatus  constructor");
 		this.s = s;
 		this.callers = callers;
 		this.pso = pso;
+		this.graceTime=graceTime;
 
 	}
 
 	@Override
 	public void run() {
 		System.out.println("Checking service status");
-		boolean status = checkServiceStatus(s);
+		boolean status = checkServiceStatus(s,graceTime);
 		if (!status && previousServiceState) {
 			for (Caller caller : callers) {
 				PlannedServiceOutage psoObj = this.pso.get(caller); // To retrieve the planned service outage object
@@ -54,14 +56,14 @@ public class ServiceStatus extends TimerTask {
 
 	}
 
-	private boolean checkServiceStatus(Service s) {
+	private boolean checkServiceStatus(Service s, int graceTime) {
 		try {
 			// Socket socket = new Socket(s.getIp(), s.getPort());
 			Socket socket = new Socket();
-			socket.connect(new InetSocketAddress(s.getIp(), s.getPort()), 2000);
+			socket.connect(new InetSocketAddress(s.getIp(), s.getPort()), graceTime); // replace this time with grace time ?
 			// System.out.println("Connection to Service (" + s.getIp() + ":" + s.getPort() + ") Established."); // should be part of logger with different level
 			logger.info("Connection to Service (" + s.getIp() + ":" + s.getPort() + ") Established.");
-
+            System.out.println("Socket is connected " + socket.isConnected());
 			socket.close();
 
 			return true;
