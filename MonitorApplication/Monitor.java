05260ec58a315b443e6f5df4c702d48b3d02cb74
Krishna Tiwari <kri1311@gmail.com>
2020-01-24 17:20:10 +0530

    Update Monitor.java

--- MonitorApplication/Monitor.java
+++ MonitorApplication/Monitor.java
@@ -23,8 +23,10 @@ public class Monitor {
 
 	private final Map<Service, List<Caller>> registerCache = new HashMap<Service, List<Caller>>();
 	private final Map<Service, Integer> hm = new HashMap<Service, Integer>();
-
-	public void register(Service s, int freq, Caller client) {
+    private final Map<Service, Timer> serviceTimer =new HashMap<Service,Timer>();
+    private final Map<Service, Integer> map = new HashMap<Service, Integer>();
+    
+	public void register(Service s, int freq, Caller client, int graceTime) {
 
 		if ((registerCache.containsKey(s)) && registerCache.get(s).contains(client))
 			return;
@@ -33,20 +35,31 @@ public class Monitor {
 			registerCache.get(s).add(client);
 
 		}
-		// To calculate the minimum frequency
+		if(serviceTimer.containsKey(s)) {
+			serviceTimer.get(s).cancel();
+			
+		}
+		// To calculate the minimum frequency 
 		hm.put(s, Math.min(freq, hm.getOrDefault(s, Integer.MAX_VALUE)));
+		
+		// To calculate the minimum graceTime
+		map.put(s, Math.min(graceTime, map.getOrDefault(s, Integer.MAX_VALUE)));
+		
 		Timer t1 = new Timer();
-		t1.schedule(new ServiceStatus(s, registerCache.get(s), psoMap.get(s)), 0, freq * 1000);
+		serviceTimer.put(s, t1);
+		t1.schedule(new ServiceStatus(s, registerCache.get(s), psoMap.get(s),map.get(s)*1000), 0, hm.get(s)*1000);
+		
 
 	}
 
 	private final Map<Service, Map<Caller, PlannedServiceOutage>> psoMap = new HashMap<Service, Map<Caller, PlannedServiceOutage>>();
 
-	public void register(Service s, int freq, Caller client, LocalTime startTime, LocalTime endTime) {
-		register(s, freq, client);
+	public void register(Service s, int freq, Caller client, LocalTime startTime, LocalTime endTime,int graceTime) {
+		register(s, freq, client,graceTime);
 		if (!((psoMap.containsKey(s))))
 			psoMap.putIfAbsent(s, new HashMap<Caller, PlannedServiceOutage>());
 		psoMap.get(s).putIfAbsent(client, new PlannedServiceOutage(startTime, endTime));
 	}
 
 }
+//Current problems : multiple instances of ServiceStatus 
